c98680ffbfa38203c225b9232dfaed501341cc97
izo0619 <isabelzhong30@gmail.com>
2023-07-18 21:15:53 -0400

    add link env

--- brax/envs/__init__.py
+++ brax/envs/__init__.py
@@ -29,6 +29,7 @@ from brax.envs import inverted_pendulum
 from brax.envs import pusher
 from brax.envs import reacher
 from brax.envs import walker2d
+from brax.envs import links
 from brax.envs.base import Env, PipelineEnv, State, Wrapper
 from brax.envs.wrappers import training
 
@@ -44,31 +45,31 @@ _envs = {
     'pusher': pusher.Pusher,
     'reacher': reacher.Reacher,
     'walker2d': walker2d.Walker2d,
+    'links': links.Links,
 }
 
 
-
 def get_environment(env_name: str, **kwargs) -> Env:
-  """Returns an environment from the environment registry.
+    """Returns an environment from the environment registry.
 
-  Args:
-    env_name: environment name string
-    **kwargs: keyword arguments that get passed to the Env class constructor
+    Args:
+      env_name: environment name string
+      **kwargs: keyword arguments that get passed to the Env class constructor
 
-  Returns:
-    env: an environment
-  """
-  return _envs[env_name](**kwargs)
+    Returns:
+      env: an environment
+    """
+    return _envs[env_name](**kwargs)
 
 
 def register_environment(env_name: str, env_class: Type[Env]):
-  """Adds an environment to the registry.
+    """Adds an environment to the registry.
 
-  Args:
-    env_name: environment name string
-    env_class: the Env class to add to the registry
-  """
-  _envs[env_name] = env_class
+    Args:
+      env_name: environment name string
+      env_class: the Env class to add to the registry
+    """
+    _envs[env_name] = env_class
 
 
 def create(
@@ -79,26 +80,26 @@ def create(
     batch_size: Optional[int] = None,
     **kwargs,
 ) -> Env:
-  """Creates an environment from the registry.
+    """Creates an environment from the registry.
 
-  Args:
-    env_name: environment name string
-    episode_length: length of episode
-    action_repeat: how many repeated actions to take per environment step
-    auto_reset: whether to auto reset the environment after an episode is done
-    batch_size: the number of environments to batch together
-    **kwargs: keyword argments that get passed to the Env class constructor
+    Args:
+      env_name: environment name string
+      episode_length: length of episode
+      action_repeat: how many repeated actions to take per environment step
+      auto_reset: whether to auto reset the environment after an episode is done
+      batch_size: the number of environments to batch together
+      **kwargs: keyword argments that get passed to the Env class constructor
 
-  Returns:
-    env: an environment
-  """
-  env = _envs[env_name](**kwargs)
+    Returns:
+      env: an environment
+    """
+    env = _envs[env_name](**kwargs)
 
-  if episode_length is not None:
-    env = training.EpisodeWrapper(env, episode_length, action_repeat)
-  if batch_size:
-    env = training.VmapWrapper(env, batch_size)
-  if auto_reset:
-    env = training.AutoResetWrapper(env)
+    if episode_length is not None:
+        env = training.EpisodeWrapper(env, episode_length, action_repeat)
+    if batch_size:
+        env = training.VmapWrapper(env, batch_size)
+    if auto_reset:
+        env = training.AutoResetWrapper(env)
 
-  return env
+    return env
---
+++ brax/envs/links.py
@@ -0,0 +1,61 @@
+from brax import base
+from brax.envs.base import PipelineEnv, State
+from jax import numpy as jp
+from etils import epath
+from brax.io import mjcf
+
+
+class Links(PipelineEnv):
+    """Trains an links to go move."""
+
+    def __init__(self, **kwargs):
+        path = epath.resource_path(
+            'brax') / 'envs/assets/2_links.xml'
+        sys = mjcf.load(path)
+        super().__init__(sys=sys, backend="positional", **kwargs)
+        self._dt = 0.02
+        self._reset_count = 0
+        self._step_count = 0
+
+    def reset(self, rng: jp.ndarray) -> State:
+        self._reset_count += 1
+        pipeline_state = base.State(
+            q=jp.zeros(1),
+            qd=jp.zeros(1),
+            x=base.Transform.create(pos=jp.zeros(2)),
+            xd=base.Motion.create(vel=jp.zeros(2)),
+            contact=None
+        )
+        obs = jp.zeros(2)
+        reward, done = jp.array(0.0), jp.array(0.0)
+        return State(pipeline_state, obs, reward, done)
+
+    def step(self, state: State, action: jp.ndarray) -> State:
+        self._step_count += 1
+        vel = state.pipeline_state.xd.vel + (action > 0) * self._dt
+        pos = state.pipeline_state.x.pos + vel * self._dt
+
+        qp = state.pipeline_state.replace(
+            x=state.pipeline_state.x.replace(pos=pos),
+            xd=state.pipeline_state.xd.replace(vel=vel),
+        )
+        obs = jp.array([pos[0], vel[0]])
+        reward = pos[0]
+
+        return state.replace(pipeline_state=qp, obs=obs, reward=reward)
+
+    @property
+    def reset_count(self):
+        return self._reset_count
+
+    @property
+    def step_count(self):
+        return self._step_count
+
+    @property
+    def observation_size(self):
+        return 2
+
+    @property
+    def action_size(self):
+        return 1
