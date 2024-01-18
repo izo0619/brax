9d154d836a407b8d3d64924bb84a441e4e9d6133
izo0619 <isabelzhong30@gmail.com>
2024-01-17 21:10:54 -0600

    revert timestep, add link cap

--- brax/envs/links.py
+++ brax/envs/links.py
@@ -18,7 +18,7 @@ class Links(PipelineEnv):
                backend='generalized',
                i=0,
                **kwargs):
-        path = epath.resource_path('brax') / f'envs/assets/hand_gen/handmade_{i+1}.xml'
+        path = epath.resource_path('brax') / f'envs/assets/5_links/rdm_{i}.xml'
         sys = mjcf.load(path)
 
         n_frames = 4
